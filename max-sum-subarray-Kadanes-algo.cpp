42ec26684a659995f985c7c7805c23a4bad45319
Ananya Patel <ananya.patel@iitgn.ac.in>
2026-05-09 16:35:46 +0530

    Enhance Kadane's algorithm to track subarray indices
    Added variables to track the start and end indices of the maximum subarray.

--- max-sum-subarray-Kadanes-algo.cpp
+++ max-sum-subarray-Kadanes-algo.cpp
@@ -4,16 +4,26 @@ public:
         long long sum=0;
         long long max_sum= LONG_MIN;
         int n=nums.size();
+        int start; // in case we have to return the subarray too.
+        int end
         for(int i=0; i<n; i++){
             if(sum<0){
+                start=i
                 sum=nums[i];
                 max_sum=max(sum,max_sum);
             }
+            
             else{
                 sum+=nums[i];
                 max_sum=max(sum,max_sum);
             }
+            if(max_sum<=sum){
+                max_sum=sum;
+                end=i;
+            
         }
+
+        //the max sum subarray is from index start to end. 
 
         return max_sum;
         
