5717e5e0500ee412f19eb57e81453eec3826a44b
Ananya Patel <ananya.patel@iitgn.ac.in>
2026-05-19 13:21:04 +0530

    Create word-search.cpp

---
+++ Recursion/word-search.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    bool dfs(vector<vector<char>>& board,string& word,int i, int j, int idx){
+        if(idx==word.length()) return true;
+        if(i<0||j<0||i>=board.size()||j>=board[0].size()||board[i][j]!=word[idx]){
+            return false;
+        }
+        char temp=board[i][j];
+        board[i][j]='#'; // to mark visited
+        bool nextchar=(dfs(board,word,i+1,j,idx+1)||dfs(board,word,i-1,j,idx+1)|dfs(board,word,i,j+1,idx+1)|dfs(board,word,i,j-1,idx+1));
+        board[i][j]=temp; // backtracking; this is to restore the board.
+        return nextchar;
+    }
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int rows=board.size();
+        int cols=board[0].size();
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(board[i][j]==word[0]){
+                    if(dfs(board,word,i,j,0)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+        
+    }
+};
