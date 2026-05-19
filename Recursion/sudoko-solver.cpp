89f8ec4c2639f1515c0eb3cfbaf48bf0f3112b40
Ananya Patel <ananya.patel@iitgn.ac.in>
2026-05-19 21:59:05 +0530

    Create sudoko-solver.cpp

---
+++ Recursion/sudoko-solver.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    bool isValid(int row, int col, char c,vector<vector<char>>& board){
+        for(int i=0;i<9;i++){
+            if(board[row][i]==c) return false;
+            if(board[i][col]==c) return false;
+            if(board[3*(row/3) + i/3][3*(col/3)+ i%3]==c) return false;
+        }
+        return true;
+    }
+    bool solve(vector<vector<char>>& board){
+        for(int row=0;row<board.size();row++){
+            for(int col=0;col<board[0].size();col++){
+                if(board[row][col]=='.'){
+                    for(int c='1';c<='9';c++){
+                        if(isValid(row,col,c,board)){
+                            board[row][col]=c;
+                            if(solve(board)){
+                                return true;
+                            }
+                            board[row][col]='.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+        
+    }
+};
