c1f4a48523f6ecc0454cacb1d82e272a256978ca
Ananya Patel <ananya.patel@iitgn.ac.in>
2026-05-23 00:36:01 +0530

    Create largest-ractangle.cpp

---
+++ stacks-and-queues/largest-ractangle.cpp
@@ -0,0 +1,31 @@
+int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int n=heights.size();
+        int maxArea=0;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && heights[st.top()]>heights[i]){
+                int element=heights[st.top()];
+                st.pop();
+                int nse=i;
+                int pse=(st.empty()) ? -1 : st.top();
+                
+                maxArea=max(maxArea, element*(nse-pse-1));
+
+            }
+            st.push(i);
+            
+
+        }
+
+        while(!st.empty()){
+            int nse=n;
+            int element=heights[st.top()];
+            st.pop();
+            int pse = (st.empty()) ? -1 : st.top();
+            maxArea=max(maxArea, element*(nse-pse-1));
+
+        }
+
+        return maxArea;
+        
+    }
