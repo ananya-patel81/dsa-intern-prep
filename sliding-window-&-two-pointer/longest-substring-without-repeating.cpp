5585dea33bd9b824a3fb66e7c79b62704d07a08e
Ananya Patel <ananya.patel@iitgn.ac.in>
2026-05-23 22:41:21 +0530

    Create longest-substring-without-repeating.cpp

---
+++ sliding-window-&-two-pointer/longest-substring-without-repeating.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n=s.length();
+        int l=0;
+        int r=0;
+        int maxlen=0;
+        vector<int> hashmap(256,-1);
+
+        while(r<n){
+            if(hashmap[s[r]]!=-1){
+                if(hashmap[s[r]]>=l){
+                    l=hashmap[s[r]]+1;
+                }
+                
+                
+            }
+            int len=r-l+1;
+            maxlen=max(maxlen,len);
+            hashmap[s[r]]=r;
+            r++;
+
+        }
+        return maxlen;
+        
+    }
+};
