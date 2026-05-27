a86b997b3a96537d04676fbb4be0b53a17001ead
Ananya Patel <ananya.patel@iitgn.ac.in>
2026-05-27 16:05:10 +0530

    Create top-view-of-bt.cpp

---
+++ Binary Trees/top-view-of-bt.cpp
@@ -0,0 +1,46 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<int> topView(Node *root) {
+        // code here
+        vector<int> ans;
+        map<int,int> nodes;
+        queue<pair<Node*, int>> q;
+        q.push({root,0});
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            Node* node=it.first;
+            int line=it.second;
+            if(nodes.find(line)==nodes.end()){
+                nodes[line]=node->data;
+            }
+            if(node->left){
+                q.push({node->left,line-1});
+            }
+            if(node->right){
+                q.push({node->right,line+1});
+            }
+        }
+        
+        for(auto p : nodes){
+            ans.push_back(p.second);
+        }
+        
+        return ans;
+    }
+};
