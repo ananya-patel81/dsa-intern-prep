2ccfd2ab1feb7a52aa624af7a53ff5caa9d1391f
Ananya Patel <ananya.patel@iitgn.ac.in>
2026-05-28 01:49:29 +0530

    Create count-nodes-in-complete-bt-logN-complexity.cpp

---
+++ Binary Trees/count-nodes-in-complete-bt-logN-complexity.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int findLeftHeight(TreeNode* root){
+        if(root==NULL) return 0;
+        int h=0;
+        TreeNode* node=root;
+        while(node){
+            h++;
+            node=node->left;
+        }
+
+        return h;
+    }
+
+    int findRightHeight(TreeNode* root){
+        if(root==NULL) return 0;
+        int h=0;
+        TreeNode* node=root;
+        while(node){
+            h++;
+            node=node->right;
+        }
+
+        return h;
+    }
+
+public:
+    int countNodes(TreeNode* root) {
+        if(root==NULL) return 0;
+
+        int leftH=findLeftHeight(root);
+        int rightH=findRightHeight(root);
+
+        if(leftH==rightH) return (1<<leftH)-1;
+
+        return 1+countNodes(root->left)+countNodes(root->right);
+        
+    }
+};
