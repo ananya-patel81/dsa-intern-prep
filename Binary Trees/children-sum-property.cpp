5555f13b4e9218e3e473b33f5450967030df9e59
Ananya Patel <ananya.patel@iitgn.ac.in>
2026-05-28 01:45:05 +0530

    Create children-sum-property.cpp

---
+++ Binary Trees/children-sum-property.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h> 
+/*************************************************************
+
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode
+    {
+    public :
+        T data;
+        BinaryTreeNode < T > *left;
+        BinaryTreeNode < T > *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+void changeTree(BinaryTreeNode < int > * root) {
+    // Write your code here.
+    if(root==NULL) return;
+    int child=0;
+    if(root->left) child+=root->left->data;
+    if(root->right) child+=root->right->data;
+    if(root->data <=child) root->data=child;
+    else{
+        if(root->left) root->left->data= root->data;
+        else if(root->right) root->right->data=root->data;
+
+    }
+    changeTree(root->left);
+    changeTree(root->right);
+
+    int tot=0; // to keep the root equal to sum of its children
+    if(root->left) tot+=root->left->data;
+    if(root->right) tot+=root->right->data;
+    if(root->left||root->right) root->data=tot;
+
+    return;
+}  
