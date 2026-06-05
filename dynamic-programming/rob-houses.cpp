cebd8a8c9134992420821741a89c950e54178a9e
Ananya Patel <ananya.patel@iitgn.ac.in>
2026-06-05 18:34:14 +0530

    Create rob-houses.cpp

---
+++ dynamic-programming/rob-houses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        //divide the problem into indexes. 
+        //do all the stuff for that index
+        //store the max;
+        //har index se i+2 jaa sakti hu
+        int n=nums.size();
+        vector<int> dp(n,0);
+        dp[0]=nums[0];
+        if(n==1) return dp[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[n-1];
+        
+    }
+};
