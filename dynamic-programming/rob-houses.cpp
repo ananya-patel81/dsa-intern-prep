bf728f69c1b92f34348ac6a15f9ae31fc5d3daaa
Ananya Patel <ananya.patel@iitgn.ac.in>
2026-06-05 18:36:37 +0530

    Add space-optimized solution for rob houses
    Implemented a space-optimized solution for the house robbing problem.

--- dynamic-programming/rob-houses.cpp
+++ dynamic-programming/rob-houses.cpp
@@ -17,3 +17,25 @@ public:
         
     }
 };
+
+//space Optimized 
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        //divide the problem into indexes. 
+        //do all the stuff for that index
+        //store the max;
+        //har index se i+2 jaa sakti hu
+        int n=nums.size();
+        int prev2=nums[0];
+        if(n==1) return nums[0];
+        int prev=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            int curr=max(prev,prev2+nums[i]);
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
+        
+    }
+};
