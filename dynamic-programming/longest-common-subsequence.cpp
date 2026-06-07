dc8033fe4008b86570075f560141584368ab8d46
Ananya Patel <ananya.patel@iitgn.ac.in>
2026-06-07 17:04:44 +0530

    Update longest-common-subsequence.cpp

--- dynamic-programming/longest-common-subsequence.cpp
+++ dynamic-programming/longest-common-subsequence.cpp
@@ -12,16 +12,16 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1)); // for index shifting 
         for(int i=0;i<=n;i++){
-            dp[i][0]=0;
+            dp[i][0]=0; 
         }
         for(int i=0;i<=m;i++){
             dp[0][i]=0;
         }
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(text1[i-1]==text2[j-1]){
+                if(text1[i-1]==text2[j-1]){ // index shifting 
                     dp[i][j]=1+dp[i-1][j-1];
                 }
                 else dp[i][j]= max(dp[i-1][j],dp[i][j-1]);
