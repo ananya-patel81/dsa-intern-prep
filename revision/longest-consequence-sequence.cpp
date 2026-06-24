edc4ea3ca16766af5b30fa3b419b13eb9d6cdf8f
Ananya Patel <ananya.patel@iitgn.ac.in>
2026-06-24 15:55:43 +0530

    Create longest-consequence-sequence.cpp

---
+++ revision/longest-consequence-sequence.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0) return 0;
+        if(n==1) return 1;
+        int longest=1;
+        unordered_set<int> st;
+        for(int num : nums){
+            st.insert(num);
+        }
+        for(auto it : st){
+            if(st.find(it-1)==st.end()){
+                int x=it;
+                int cnt=1;
+                while(st.find(x+1)!=st.end()){
+                    x=x+1;
+                    cnt++;
+                }
+                longest=max(longest,cnt);
+            }
+        }
+
+        return longest;
+    }
+};
