4111da3442755a038451ac49f215d5afdffa417b
Ananya Patel <ananya.patel@iitgn.ac.in>
2026-07-09 15:56:23 +0530

    Add Bellman-Ford algorithm implementation
    Implement Bellman-Ford algorithm for shortest paths.

---
+++ revision/graph/bellman-ford-algo.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+        vector<int> dist(V,1e8);
+        dist[src]=0;
+        for(int i=0;i<V;i++){
+            for(auto& e : edges){
+                int u = e[0];
+                int v = e[1];
+                int wt = e[2];
+                if(dist[u]!=1e8 && dist[u]+wt<dist[v]){
+                    dist[v]= dist[u]+wt;
+                }
+            }
+        }
+        
+        for(auto& e : edges){
+            int u=e[0];
+            int v=e[1];
+            int wt=e[2];
+            if(dist[u]!=1e8 && dist[u]+wt<dist[v]){
+                return {-1};
+            }
+        }
+        
+        return dist;
+        
+    }
+};
