fb5082fe377e264c110bece7f84ab5a96f51b92e
Ananya Patel <ananya.patel@iitgn.ac.in>
2026-07-10 16:17:37 +0530

    Add LCS length and print function
    Implement function to find and print the longest common subsequence.

---
+++ revision/dp/lcs-length-and-print.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string findLCS(int n, int m,string &s1, string &s2){
+    vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if(s1[i-1]==s2[j-1]){
+                dp[i][j]=1+dp[i-1][j-1];
+            }
+            else{
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+
+    int len = dp[n][m];
+    int index= len-1;
+    int i=n, j=m;
+    string ans="";
+    for(int k=0;k<len;k++){
+        ans+='$';
+    }
+    while(i>0 && j>0){
+        if(s1[i-1]==s2[j-1]){
+            ans[index]=s1[i-1];
+            index--;
+            i--;
+            j--;
+        }
+        else{
+            if(dp[i-1][j]>dp[i][j-1]){
+                i--;
+            }
+            else{
+                j--;
+            }
+        }
+    }
+
+    return ans;
+
+}
