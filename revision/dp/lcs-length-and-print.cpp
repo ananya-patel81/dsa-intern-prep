c27c31c2e82d04643c1f752cc6de2ee0e0040800
Ananya Patel <ananya.patel@iitgn.ac.in>
2026-07-10 16:42:49 +0530

    Update lcs-length-and-print.cpp

--- revision/dp/lcs-length-and-print.cpp
+++ revision/dp/lcs-length-and-print.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//iske neeche khali DP table banaane ke liye tabulation and space optimized solution bhi hai 
 
 string findLCS(int n, int m,string &s1, string &s2){
     vector<vector<int>> dp(n+1,vector<int>(m+1,0));
@@ -42,3 +43,94 @@ string findLCS(int n, int m,string &s1, string &s2){
     return ans;
 
 }
+
+//Space optimized
+class Solution {
+    int f(int ind1, int ind2,vector<vector<int>>& dp, string& text1, string& text2 ){
+        if(ind1==0 || ind2==0) return 0;
+        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+        if(text1[ind1-1]==text2[ind2-1]){
+            return dp[ind1][ind2]=1+f(ind1-1, ind2-1, dp,text1, text2);
+        }
+        return dp[ind1][ind2]= max(f(ind1-1,ind2, dp,text1,text2),f(ind1, ind2-1,dp,text1, text2));
+    }
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.length();
+        int m = text2.length();
+        vector<int> prev(m+1,0);
+        vector<int> curr(m+1,0);
+        // int temp = f(n,m,dp,text1,text2);
+        // return temp;
+        for(int i=0;i<=m;i++){
+            prev[i]=0;
+        }
+        
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1]==text2[j-1]){
+                    curr[j]=1+prev[j-1];
+                }
+                else{
+                    curr[j]=max(prev[j],curr[j-1]);
+                }
+            }
+            prev=curr;
+        }
+
+        return prev[m];
+        
+    }
+};
+
+//Tabulation, and recursive duntion bhi hai
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.length();
+        int m = text2.length();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        // int temp = f(n,m,dp,text1,text2);
+        // return temp;
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=0;i<=m;i++){
+            dp[0][i]=0;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+        
+    }
+};
+
+class Solution {
+    int f(int ind1, int ind2,vector<vector<int>>& dp, string& text1, string& text2 ){
+        if(ind1==0 || ind2==0) return 0;
+        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+        if(text1[ind1-1]==text2[ind2-1]){
+            return dp[ind1][ind2]=1+f(ind1-1, ind2-1, dp,text1, text2);
+        }
+        return dp[ind1][ind2]= max(f(ind1-1,ind2, dp,text1,text2),f(ind1, ind2-1,dp,text1, text2));
+    }
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.length();
+        int m = text2.length();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return f(n,m,dp,text1,text2);
+        
+    }
+};
